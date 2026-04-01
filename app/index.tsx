2117604dfc31809d4179c9e788e3b2b3ec55d39e
Jônata Marcelino <jonata.marcelino83@gmail.com>
2026-04-01 19:02:45 -0300

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text>Teste</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
