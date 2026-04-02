c0cca9ed9e6f65f42db71fe80c8568bcdf929c79
Jônata Marcelino <jonata.marcelino83@gmail.com>
2026-04-01 23:14:12 -0300

    ajustes na SafeAreaView

--- assets/styles.tsx
+++ assets/styles.tsx
@@ -68,12 +68,10 @@ export const BackgroundApp = styled(SafeAreaView)`
 export const ContainerHeader = styled(SafeAreaView)`
   flex-direction: row;
   align-items: center;
-  justify-content: flex-start;
-  margin-top: 30px;
+  width: 100%;
+  padding: 0;
   margin-left: 15px;
   margin-bottom: 15px;
-  width: 100%;
-  max-height: 60px;
 `;
 
 export const TitleHeader = styled.Text`
